3d25671137b4821966c637bd0492ccbf1a2f7b93
Cortina17 <diego.cortinagarcia@gmail.com>
2022-01-28 21:31:55 +0100

    Beta version
    Added readme file and corrected and error which caused the edit button not to work properly

---
+++ README.md
@@ -0,0 +1,11 @@
+# Simple To Do List
+
+To do list for task and to do notes.
+
+## Description
+
+This app is completely made with HTML and vanilla Javascript.
+
+### Author
+
+@Cortina17
--- main.js
+++ main.js
@@ -1,9 +1,9 @@
-window.addEventListener('load', () => {
+window.addEventListener("load", () => {
     const form = document.querySelector("#new-task-form");
     const input = document.querySelector("#new-task-input");
     const list_el = document.querySelector("#tasks");
 
-    form.addEventListener('submit', (e) => {
+    form.addEventListener("submit", (e) => {
         e.preventDefault();
 
         const task = input.value;
@@ -23,22 +23,22 @@ window.addEventListener('load', () => {
 
         const task_input_el = document.createElement("input");
         task_input_el.classList.add("text");
-        task_input_el.type = 'text';
+        task_input_el.type = "text";
         task_input_el.value = task;
-        task_input_el.setAttribute('readonly', 'readonly');
+        task_input_el.setAttribute("readonly", "readonly");
 
         task_content_el.appendChild(task_input_el);
 
-        const task_actions_el = document.createElement('div');
-        task_actions_el.classList.add('actions');
+        const task_actions_el = document.createElement("div");
+        task_actions_el.classList.add("actions");
 
-        const task_edit_el = document.createElement('button');
-        task_edit_el.classList.add('edit');
-        task_edit_el.innerText = "Editar";
+        const task_edit_el = document.createElement("button");
+        task_edit_el.classList.add("edit");
+        task_edit_el.innerHTML = "Editar";
 
-        const task_delete_el = document.createElement('button');
-        task_delete_el.classList.add('delete');
-        task_delete_el.innerHTML = "Borrar";
+        const task_delete_el = document.createElement("button");
+        task_delete_el.classList.add("delete");
+        task_delete_el.innerText = "Borrar";
 
         task_actions_el.appendChild(task_edit_el);
         task_actions_el.appendChild(task_delete_el);
@@ -47,21 +47,22 @@ window.addEventListener('load', () => {
 
         list_el.appendChild(task_el);
 
-        input.value = '';
+        input.value = "";
 
-        task_edit_el.addEventListener('click', (e) => {
-            if (task_edit_el.innerText.toLowerCase() == "edit") {
-                    task_edit_el.innerText = "Guardar";
-                    task_input_el.removeAttribute("readonly");
-                    task_input_el.focus();
+        task_edit_el.addEventListener('click', () => {
+            if (task_edit_el.innerText.toLowerCase() == "editar") {
+                task_input_el.removeAttribute("readonly");
+                task_input_el.focus();
+                console.log("save");
+                task_edit_el.innerText = "Guardar";
             } else {
-                task_edit_el.innerText = "Editar";
                 task_input_el.setAttribute("readonly", "readonly");
+                task_edit_el.innerText = "Editar";
             }
         });
 
-        task_delete_el.addEventListener('click', (e) => {
+        task_delete_el.addEventListener("click", (e) => {
             list_el.removeChild(task_el);
         });
-});
+    });
 });
